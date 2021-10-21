0a85ed351f53a9f9c1ee4935bbe68e151bd21332
abondar24 <abondar1992@gmail.com>
2021-10-21 19:59:05 +0200

    build plugins

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Medicine search
+
+
+## Build and run
+
+```yaml
+./gradlew clean build
+
+java -jar build/libs/MedicineSearch-1.0-SNAPSHOT-all.jar 
+```
---
+++ src/main/java/org/abondar/industrial/medsearch/Main.java
@@ -0,0 +1,8 @@
+package org.abondar.industrial.medsearch;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("App is up");
+  }
+}
